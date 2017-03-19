bd8988544fb9d088b70b7f33217a530cfbf0fd3d
Rune Ronning <epkrrgo@gmail.com>
2017-03-19 10:04:40 +0100

    Finalized the reveal on scroll feature.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,10 @@
 /*jslint node: true */
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+
+var fI = new RevealOnScroll($(".feature-item"), "85%");
+var tM = new RevealOnScroll($(".testimonial"), "60%");
+
