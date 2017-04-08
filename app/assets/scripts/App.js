bd1b8bd5b22e2d3950f1254c9b316f5347a72c59
Rune Ronning <epkrrgo@gmail.com>
2017-04-08 23:15:49 +0200

    Added a few items along the way.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,9 +2,12 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 
 var fI = new RevealOnScroll($(".feature-item"), "85%");
 var tM = new RevealOnScroll($(".testimonial"), "60%");
+
+var stickyHeader = new StickyHeader();
 
